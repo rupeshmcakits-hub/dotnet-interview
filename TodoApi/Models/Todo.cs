70e8ec1b88699cb8236d8a3eee22f744ffc86120
rupeshmcakits-hub <rupeshmcakits@gmail.com>
2026-02-27 16:20:54 +0530

    Update Todo.cs

--- TodoApi/Models/Todo.cs
+++ TodoApi/Models/Todo.cs
@@ -1,11 +1,35 @@
+using System;
+
 namespace TodoApi.Models
 {
+    /// <summary>
+    /// Represents a Todo item with details and status.
+    /// </summary>
     public class Todo
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the Todo item.
+        /// </summary>
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the Todo item.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the Todo item.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Todo item is completed.
+        /// </summary>
         public bool IsCompleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date and time of the Todo item.
+        /// </summary>
         public DateTime CreatedAt { get; set; }
     }
 }
